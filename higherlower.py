b62b181bb0b711e05076a86eb45c6d6b0e3213ef
Mohamed Yazidi <mohamedpk@hotmail.de>
2024-12-11 21:30:44 +0100

    first commit

---
+++ higherlower.py
@@ -0,0 +1,13 @@
+"""
+higher lower consolegame
+"""
+
+from pyfiglet import Figlet
+
+f = Figlet(font="slant")
+print(f.renderText("Welcome to \n HIGHER LOWER"))
+
+def game_loop():
+    running = True
+
+        
